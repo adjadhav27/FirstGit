46f33419bcfe9d4d33102b879fb9fe9a2494cd06
adjadhav27 <jadhav.avinash0@gmail.com>
2023-12-16 14:46:46 +0530

    getElementsByTagName task

---
+++ DOM/getElementsByTagName_task.js
@@ -0,0 +1,6 @@
+const listElement = document.getElementsByTagName('li');
+listElement[4].style.color= 'blue';
+
+for(let i=0; i<listElement.length; i++){
+  listElement[i].style.fontStyle = 'italic';
+}
