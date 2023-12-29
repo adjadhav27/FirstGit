abef32ad5921e04d7d3d31715f07471e4e86e89f
adjadhav27 <jadhav.avinash0@gmail.com>
2023-12-29 12:08:28 +0530

    DeleteFunctionality_task_js

---
+++ DOM/DeleteFunctionality/index.js
@@ -0,0 +1,34 @@
+function handleFormSubmit(event) 
+{
+    event.preventDefault();
+    const userDetails = 
+    {
+      username: event.target.username.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+    };
+    localStorage.setItem(userDetails.email, JSON.stringify(userDetails));
+    displayUserOnScreen(userDetails);
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+}
+function displayUserOnScreen(userDetails) 
+{
+    const userItem = document.createElement("li");
+    userItem.appendChild(
+      document.createTextNode(
+       ` ${userDetails.username} - ${userDetails.email} - ${userDetails.phone}`
+      )
+    );
+    const deleteBtn = document.createElement("button");
+    deleteBtn.appendChild(document.createTextNode("Delete"));
+    userItem.appendChild(deleteBtn);
+    const userList = document.querySelector("ul");
+    userList.appendChild(userItem);
+    deleteBtn.addEventListener("click", function (event) 
+    {
+      userList.removeChild(event.target.parentElement);
+      localStorage.removeItem(userDetails.email);
+    });
+}
