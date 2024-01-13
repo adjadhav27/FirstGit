8e5ca62743ec289cb0c4988226d9d1911afa0b2a
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-13 14:20:39 +0530

    AppointmentCallAPI2_Task_JS

---
+++ AppointmentCallAPI2/index.js
@@ -0,0 +1,57 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+  
+    const user = {
+      username: event.target.username.value,
+      email: event.target.email.value,
+      phone: event.target.phone.value,
+    };
+  
+    axios
+      .post('https://crudcrud.com/api/8cca9a0745bf49c49e12187e31042c34/appointmentData', user)
+      .then((response) => {
+        displayUser(response.data);
+      })
+      .catch((error) => {
+        document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong </h4>";
+        console.log(error);
+      });
+  
+    document.getElementById("username").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phone").value = "";
+  }
+  
+  async function displayUsers() {
+    try {
+      const response = await axios.get('https://crudcrud.com/api/8cca9a0745bf49c49e12187e31042c34/appointmentData');
+      for (let i = 0; i < response.data.length; i++) {
+        displayUser(response.data[i]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  function displayUser(user) {
+    const newElement = document.createElement("li");
+    newElement.appendChild(document.createTextNode(` ${user.username} - ${user.email} - ${user.phone}`));
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    newElement.appendChild(deleteButton);
+  
+    const editButton = document.createElement("button");
+    editButton.appendChild(document.createTextNode("Edit"));
+    newElement.appendChild(editButton);
+  
+    const userList = document.querySelector("ul");
+    userList.appendChild(newElement);
+  
+  }
+  
+  // Initializing the displayUsers function when the DOM content is loaded
+  document.addEventListener('DOMContentLoaded', () => {
+    displayUsers();
+  });
+  
