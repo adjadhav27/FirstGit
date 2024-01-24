0161a7efc54e5e599d40f4baec82de6aa5202cc0
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-24 20:40:23 +0530

    React_Dynamic_Variable_JS_Task

--- React-First-App/src/components/ExpenseItem.js
+++ React-First-App/src/components/ExpenseItem.js
@@ -1,20 +1,24 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const LocationOfExpenditure = 'Mumbai';
+  const foodName = 'Indian';
+  const amount = 249.50;
   return (
     <div>
       <div className='expense-item'>
-      <div className="expense-item__description"><h2>Food</h2></div>
+      <div className="expense-item__description"><h2>{foodName}</h2></div>
+      <div className="expense-item__description"><h2>{LocationOfExpenditure}</h2></div>
+      <div className="expense-item__price">${amount}</div>
+      </div>
+      {/* <div className='expense-item'>
+      <div className="expense-item__description"><h2>Petrol</h2></div>
       <div className="expense-item__price">Rs.10</div>
       </div>
       <div className='expense-item'>
       <div className="expense-item__description"><h2>Food</h2></div>
       <div className="expense-item__price">Rs.10</div>
-      </div>
-      <div className='expense-item'>
-      <div className="expense-item__description"><h2>Food</h2></div>
-      <div className="expense-item__price">Rs.10</div>
-    </div>
+      </div> */}
     </div>
   );
 }
