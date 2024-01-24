0c1b1e8417f79e429618cd162ecce7392b8aaef4
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-24 19:39:52 +0530

    React-First-App_Task1

---
+++ React-First-App/src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h4>Food Rs 10</h4>
+      </div>
+      <div>
+        <h4>Petrol Rs 100</h4>
+      </div>
+      <div>
+        <h4>Movies Rs 200</h4>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
