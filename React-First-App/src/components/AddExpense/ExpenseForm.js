dbb34f3bde0b505c10cd3b85d292c48cb23d7b4a
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-26 03:31:01 +0530

    Add_Expense_Console_Log_Task

--- React-First-App/src/components/AddExpense/ExpenseForm.js
+++ React-First-App/src/components/AddExpense/ExpenseForm.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 const ExpenseForm = () => {
+
+    function updateTitle(event){
+        console.log(event.target.value);
+    }
+
+    function updateAmount(event){
+        console.log(event.target.value);
+    }
+
+    function updateDate(event){
+        console.log(event.target.value);
+    }
+
     return (
         <form>
                     <label for="title">Expense Title</label>
-                    <input type='text' id="title"/><br/><br/>
+                    <input type='text' id="title" onChange={updateTitle}/><br/><br/>
                     <label for="amount">Expense Amount</label>
-                    <input type='number' id="amount"/><br/><br/>
+                    <input type='number' id="amount" onChange={updateAmount}/><br/><br/>
                     <label for="date">Expense Date</label>
-                    <input type='date' id="date"/><br/><br/>
+                    <input type='date' id="date" onChange={updateDate}/><br/><br/>
                 <button type='submit'>Add New Expense</button><br/><br/>
         </form>
     );
