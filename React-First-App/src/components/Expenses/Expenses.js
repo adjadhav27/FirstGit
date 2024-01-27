aca26056f6435c349e253dbe79025b4770c81481
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-28 02:47:17 +0530

    Expense_Filter_Task

--- React-First-App/src/components/Expenses/Expenses.js
+++ React-First-App/src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import React from 'react';
+import React, {useState} from 'react';
 import './Expenses.css';
+import ExpenseFilter from './ExpenseFilter';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
   const {items} = props;
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       {items.map((object, index)=>(
         <ExpenseItem
         title={object.title}
