c19b70c19ced095684c210dcdc4453c7b9cfa621
adjadhav27 <jadhav.avinash0@gmail.com>
2024-01-28 05:22:55 +0530

    Single_Expense_Show_MSG

--- React-First-App/src/components/Expenses/ExpenseList.js
+++ React-First-App/src/components/Expenses/ExpenseList.js
@@ -6,6 +6,9 @@ const ExpensesList = props => {
     if(props.items.length===0){
         return <h2 className='expenses-list__fallback'>Found no expenses!</h2>
     }
+    if(props.items.length===1){
+        return <h2 className='expenses-list__fallback'>Only single Expense here. Please add more...</h2>
+    }
 
     return (
         <ul className='expenses-list'> 
