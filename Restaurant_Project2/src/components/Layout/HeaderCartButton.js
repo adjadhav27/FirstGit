6c928092b06e710ed69913735088d72573fafdbc
adjadhav27 <jadhav.avinash0@gmail.com>
2024-02-24 11:00:11 +0530

    Modal_Cart_task

--- Restaurant_Project2/src/components/Layout/HeaderCartButton.js
+++ Restaurant_Project2/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,15 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
-
 const HeaderCartButton = (props) => {
-
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>0</span>
+      <span className={classes.badge}>1</span>
     </button>
   );
 };
-
 export default HeaderCartButton;
